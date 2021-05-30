c0d071d68e6ae6d23ede246009385432d93f4d7d
Davian Yang <kothantyarzarhein@gmail.com>
2021-05-30 19:48:13 +0630

    feat: add initial backend API (#10)
    * [Add] Backend service
    
    * chore: update gitignore and api files
    
    * chore: move requirements.txt into api
    
    * fix: pin starlette to 0.14.2
    
    Co-authored-by: ydcjeff <32727188+ydcjeff@users.noreply.github.com>

--- README.md
+++ README.md
@@ -25,3 +25,19 @@ Serve:
 ```sh
 pnpm serve
 ```
+
+# FastAPI Backend
+
+Install packages from `requirements.txt`
+
+```sh
+pip install -r requirements.txt
+```
+
+Run Uvicorn
+
+```sh
+uvicorn api.main:app --reload
+```
+
+Check api from `http://127.0.0.1:8000/docs`
---
+++ api/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from api.config import settings, Settings
+from api.routes import route as api_router
+
+
+def get_application(settings: Settings = settings) -> FastAPI:
+    application = FastAPI(
+        title=settings.PROJECT_NAME, debug=settings.DEBUG, version=settings.VERSION
+    )
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.ALLOWED_HOSTS or ["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    application.include_router(api_router, prefix=settings.API_PREFIX)
+
+    return application
+
+
+app = get_application()
