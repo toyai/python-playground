df75f2f7fd6e4344fb9a1429c113a70d5ecda0ec
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-05-31 01:15:27 +0630

    feat: add embeddable code button & edit on playground link for mobile
    fix #8

---
+++ src/header/EditOnPlayground.vue
@@ -0,0 +1,21 @@
+<template>
+  <a
+    :href="playgroundURL"
+    class="mx-1"
+    target="_blank"
+    rel="noopener noreferrer"
+    title="Edit the code on Playground"
+    v-if="!inSameWindow"
+  >
+    Edit on Playground
+  </a>
+</template>
+
+<script>
+import { playgroundURL, inSameWindow } from './utils.js'
+export default {
+  setup() {
+    return { playgroundURL, inSameWindow }
+  }
+}
+</script>
