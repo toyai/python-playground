cf4918b556fa852c054247f6428fb45a74c8f64e
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-05-30 18:21:49 +0630

    chore: update description and add og image

--- windi.config.js
+++ windi.config.js
@@ -6,11 +6,6 @@ export default defineConfig({
     extend: {
       cursor: {
         'ew-resize': 'ew-resize'
-      },
-      fontFamily: {
-        sans: [
-          '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,ui-sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"'
-        ]
       }
     }
   }
