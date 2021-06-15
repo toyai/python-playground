05fb3ba73e11c79024059307102e9edb2d9f4499
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2021-06-15 21:31:26 +0630

    feat: set frontend and backend url from env var (#58)
    * feat: set frontend and backend url from env var
    
    * chore: change env var for custom domain

--- api/config.py
+++ api/config.py
@@ -4,7 +4,9 @@ from typing import List
 
 from pydantic import BaseSettings
 
-PR_NUMBER = os.environ.get("HEROKU_PR_NUMBER", None)
+PR_NUMBER = os.getenv("HEROKU_PR_NUMBER")
+PROD_SITE_NAME = os.getenv("PG_FRONTEND_SITE_NAME")
+CUSTOM_DOMAIN_URL = os.getenv("CUSTOM_DOMAIN_URL")
 
 
 class Settings(BaseSettings):
@@ -20,13 +22,15 @@ class Settings(BaseSettings):
     DEBUG: bool = False
     API_PREFIX: str = f"/api/v{MAJOR}"
     ALLOWED_HOSTS: List[str] = [
-        "https://python-playground.netlify.app",
+        f"https://{PROD_SITE_NAME}.netlify.app",
         "http://localhost:3000",
     ]
     if PR_NUMBER:
         ALLOWED_HOSTS.append(
-            f"https://deploy-preview-{PR_NUMBER}--python-playground.netlify.app"
+            f"https://deploy-preview-{PR_NUMBER}--{PROD_SITE_NAME}.netlify.app"
         )
+    if CUSTOM_DOMAIN_URL:
+        ALLOWED_HOSTS.append(CUSTOM_DOMAIN_URL)
 
 
 @lru_cache()
