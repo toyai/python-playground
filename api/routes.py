b64e13695d478afc22648277fdd766be147ff3ae
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-06-15 20:31:26 +0630

    refactor: change to with statement for StringIO

--- api/routes.py
+++ api/routes.py
@@ -12,12 +12,11 @@ input = Body(...)
 
 @route.post("/")
 def index(input: InputCode = input):
-    f = io.StringIO()
-    with redirect_stdout(f):
-        try:
-            exec(input.source)
-        except Exception:
-            return traceback.format_exc()
-    out = f.getvalue()
-    f.close()
-    return out
+    with io.StringIO() as f:
+        with redirect_stdout(f):
+            try:
+                exec(input.source)
+            except Exception:
+                return traceback.format_exc()
+        result = f.getvalue()
+    return result
