e00c373ec695c3f8357202d8513176bcbc510f01
Davian Yang <kothantyarzarhein@gmail.com>
2021-06-19 12:44:14 +0630

    feat: log api docs in dev mode (#59)

--- api/main.py
+++ api/main.py
@@ -1,9 +1,22 @@
+import logging
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import PlainTextResponse
 
 from api.config import Settings, settings
 from api.routes import route as api_router
+
+URL = "http://127.0.0.1:8000"
+cyan = "\x1b[36m"
+end = "\x1b[39m"
+
+logger = logging.getLogger("uvicorn")
+
+
+def show_api_docs():
+    logger.info(f"Swagger API docs: {cyan}{URL}/docs{end}")
+    logger.info(f"Redoc API docs: {cyan}{URL}/redoc{end}")
 
 
 def get_application(settings: Settings = settings) -> FastAPI:
@@ -12,6 +25,7 @@ def get_application(settings: Settings = settings) -> FastAPI:
         debug=settings.DEBUG,
         version=settings.VERSION,
         default_response_class=PlainTextResponse,
+        on_startup=[show_api_docs],
     )
 
     application.add_middleware(
